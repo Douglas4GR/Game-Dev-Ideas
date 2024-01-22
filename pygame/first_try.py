ce60b49a736db406721941be12a0b0c1ca0905be
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-01-22 00:58:52 -0300

    aprendendo a colocar frames e ajustar a velocidade de objetos

--- pygame/first_try.py
+++ pygame/first_try.py
@@ -1,17 +1,21 @@
 import pygame
 
 pygame.init()
+pygame.display.set_caption("Meu Jogo Pygame")
 
 
 largura, altura = 800, 600
-x, y = 0, 0
 tela = pygame.display.set_mode((largura, altura))
 
-pygame.display.set_caption("Meu Jogo Pygame")
+x = largura /2
+y = 0
+
+relogio = pygame.time.Clock()
 
 #todo jogo deve rodar num loop
 running = True
 while running:
+    relogio.tick(30) #quantidade de frames por segundo
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -21,6 +25,8 @@ while running:
     tela.fill((255, 255, 255)) #preenche a cor de fundo
 
     pygame.draw.rect(tela, (255,0,0), (x,y,50,60)) #os parametros são organizados da seguinte maneira: onde vai ficar, qual cor vai ter, depois em qual posição ele começa (X e Y) e por fim seu tamanho
+    if y >= altura:
+        y = 0
     y = y + 1 
     pygame.draw.rect(tela, (0,255,0), (y,x,40,50))
     pygame.draw.circle(tela, (0,0,120), (300,260), 40)
