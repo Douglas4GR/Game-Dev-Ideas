8a3fc84d7f457f14dd5bd8b64fceb1753f0f27d9
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-01-25 23:18:12 -0300

    pega pega entre retângulos

--- pygame/aprendendo_cores_e_formas.py
+++ pygame/aprendendo_cores_e_formas.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 #from sys import exit
+from random import randint
 
 pygame.init()
 
@@ -9,6 +10,9 @@ largura, altura = 800, 600
 x = largura /2
 y = altura /2
 
+x_verde = randint(40, 760)
+y_verde = randint(40, 560)
+
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Meu Jogo Pygame")
 relogio = pygame.time.Clock()
@@ -16,8 +20,12 @@ relogio = pygame.time.Clock()
 #todo jogo deve rodar num loop
 running = True 
 while running:
-    relogio.tick(60) #quantidade de frames por segundo
+    relogio.tick(30) #quantidade de frames por segundo
     for event in pygame.event.get():
+        
+        tela.fill((255, 255, 255)) #preenche a cor de fundo
+
+
         if event.type == pygame.QUIT:
             running = False
         if pygame.key.get_pressed()[K_a]:
@@ -31,10 +39,13 @@ while running:
 
 
 
-    tela.fill((255, 255, 255)) #preenche a cor de fundo
-
-    pygame.draw.rect(tela, (255,0,0), (x,y,50,60)) #os parametros são organizados da seguinte maneira: onde vai ficar, qual cor vai ter, depois em qual posição ele começa (X e Y) e por fim seu tamanho
-
+        ret_vermelho = pygame.draw.rect(tela, (255,0,0), (x,y,50,60)) #os parametros são organizados da seguinte maneira: onde vai ficar, qual cor vai ter, depois em qual posição ele começa (X e Y) e por fim seu tamanho
+        ret_verde = pygame.draw.rect(tela, (0,255,0), (x_verde,y_verde,40,50))
+        if ret_vermelho.colliderect(ret_verde):
+            x_verde = randint(40, 760)
+            y_verde = randint(40, 560)
+        
+        pygame.display.update()
     pygame.display.flip()
 
 pygame.quit()
