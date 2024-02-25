57f699b32d8069ff70c930a0b1baa59d84e4af80
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-02-25 00:05:16 -0300

    ainda tenho que ajustar o tamanho da cobra, da tela, a velocidade e os frames para otimizar a experiencia

--- pygame/cobrinha/jogo_da_cobra.py
+++ pygame/cobrinha/jogo_da_cobra.py
@@ -12,7 +12,7 @@ som_colisao = pygame.mixer.Sound('pygame/projeto_com_sons/colisao.wav')
 
 
 #TELA DO JOGO
-largura, altura = 800, 600 #tamanho da tela
+largura, altura = 800, 800 #tamanho da tela
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Meu Jogo Pygame")
 relogio = pygame.time.Clock()
@@ -36,7 +36,7 @@ fonte = pygame.font.SysFont('arial', 48, True, False)
 
 
 #MOVIMENTAÇÃO DA COBRA
-velocidade = 7 #velocidade da cobra
+velocidade = 50 #velocidade da cobra
 x_direcao = velocidade
 y_direcao = 0
 
@@ -62,14 +62,15 @@ def aumenta_cobra(lista_cobra, cobra):
         #XY = [x, y]
         #XY[0] = x
         #XY[1] = y
-        pygame.draw.rect(tela, verde, (XY[0], XY[1], 20, 20))
+        # o tamanho abaixo é a área visual da cobra
+        pygame.draw.rect(tela, verde, (XY[0], XY[1], 100, 100))
 
 
 #função que detecta se cobra colidiu com a parede
 def colisao_parede(x_cobra, y_cobra):
-    if x_cobra < 0 or x_cobra > largura - 20:
+    if x_cobra < 0 or x_cobra > largura - 100:
         return True
-    if y_cobra < 0 or y_cobra > altura - 20:
+    if y_cobra < 0 or y_cobra > altura - 100:
         return True
     return False
 
@@ -88,7 +89,7 @@ def jogo():
 #todo jogo deve rodar num loop
 running = True
 while running:
-    relogio.tick(30) #quantidade de frames por segundo
+    relogio.tick(10) #quantidade de frames por segundo
     tela.fill(preto) #preenche a cor de fundo
     mensagem = f'Pontuação: {pontuacao}'
     texto_formatado = fonte.render(mensagem, True, branco) #false é para suavizar as bordas
@@ -128,8 +129,9 @@ while running:
     x_cobra += x_direcao
     y_cobra += y_direcao
 
-    cobra = pygame.draw.rect(tela, verde, (x_cobra,y_cobra,20,20)) #os parametros são organizados da seguinte maneira: onde vai ficar, qual cor vai ter, depois em qual posição ele começa (X e Y) e por fim seu tamanho
-    maca = pygame.draw.rect(tela, vermelho, (x_vermelho,y_vermelho,30,30))
+    #o tamanho abaixo é a área fisica da cobra, o espaço que realmente ocupa
+    cobra = pygame.draw.rect(tela, verde, (x_cobra,y_cobra,100,100)) #os parametros são organizados da seguinte maneira: onde vai ficar, qual cor vai ter, depois em qual posição ele começa (X e Y) e por fim seu tamanho
+    maca = pygame.draw.rect(tela, vermelho, (x_vermelho,y_vermelho,110,110))
 
 
     if cobra.colliderect(maca): #se a cobra colidir com a maçã
