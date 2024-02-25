7d1f6e416932aa6ee2114df12db0a2ecd0dda804
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-02-24 23:51:36 -0300

    jogo da cobrinha pronto :)

--- pygame/cobrinha/jogo_da_cobra.py
+++ pygame/cobrinha/jogo_da_cobra.py
@@ -16,6 +16,8 @@ largura, altura = 800, 600 #tamanho da tela
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Meu Jogo Pygame")
 relogio = pygame.time.Clock()
+x_meio = int(largura / 2)
+y_meio = int(altura / 2)
 
 
 #CORES DO JOGO
@@ -34,18 +36,20 @@ fonte = pygame.font.SysFont('arial', 48, True, False)
 
 
 #MOVIMENTAÇÃO DA COBRA
-velocidade = 5 #velocidade da cobra
+velocidade = 7 #velocidade da cobra
 x_direcao = velocidade
 y_direcao = 0
+
+
+#OUTROS DETALHES DA COBRA
+lista_cobra = [] #lista que vai armazenar a posição da cobra
+comprimento_inicial = 10   #comprimento inicial da cobra
+morreu = False
 
 
 #posição inicial da cobra
 x_cobra = int(largura /2)
 y_cobra = int(altura /2)
-lista_cobra = [] #lista que vai armazenar a posição da cobra
-comprimento_inicial = 3  #comprimento inicial da cobra
-
-
 #posição inicial da maçã
 x_vermelho = randint(40, 760)
 y_vermelho = randint(40, 560)
@@ -61,6 +65,25 @@ def aumenta_cobra(lista_cobra, cobra):
         pygame.draw.rect(tela, verde, (XY[0], XY[1], 20, 20))
 
 
+#função que detecta se cobra colidiu com a parede
+def colisao_parede(x_cobra, y_cobra):
+    if x_cobra < 0 or x_cobra > largura - 20:
+        return True
+    if y_cobra < 0 or y_cobra > altura - 20:
+        return True
+    return False
+
+
+#INICIO DO JOGO
+def jogo():
+    global morreu, pontuacao, x_cobra, y_cobra, x_vermelho, y_vermelho, lista_cobra, comprimento_inicial
+    lista_cobra = [] 
+    comprimento_inicial = 5
+    morreu = False
+    x_cobra = x_meio
+    y_cobra = y_meio
+    x_vermelho = randint(40, 760)
+    y_vermelho = randint(40, 560)
 
 #todo jogo deve rodar num loop
 running = True
@@ -121,6 +144,48 @@ while running:
 
     lista_cobra.append(lista_cabeca) #adiciona a cabeça da cobra na lista da cobra
 
+    if lista_cobra.count(lista_cabeca) > 1 or colisao_parede(x_cobra,y_cobra) == True: #se a cobra colidir com ela mesma ou com a parede
+        # Mensagem de morte
+        mensagem_pontuacao = f'Pontuação: {pontuacao}'
+        mensagem_aperte_espaco = 'Aperte espaço para jogar novamente'
+
+        # Renderizando as mensagens
+        texto_pontuacao = fonte.render(mensagem_pontuacao, True, branco)
+        texto_aperte_espaco = fonte.render(mensagem_aperte_espaco, True, branco)
+        retang_pontuacao = texto_pontuacao.get_rect()
+        retang_aperte_espaco = texto_aperte_espaco.get_rect()
+
+        som_colisao.play()
+        morreu = True
+        tela.fill(verde_escuro)
+        while morreu:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    morreu = False
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == K_SPACE:
+                        jogo()
+            # Exibindo as mensagens na tela
+            retang_pontuacao.center = (x_meio, y_meio - 60)
+            retang_aperte_espaco.center = (x_meio, y_meio)
+            tela.blit(texto_pontuacao, retang_pontuacao) 
+            tela.blit(texto_aperte_espaco, retang_aperte_espaco)
+            pygame.display.update()
+
+    '''
+    condições para a cobra atravessar a parede
+    if x_cobra > largura:
+        x_cobra = 0
+    if x_cobra < 0:
+        x_cobra = largura
+    if y_cobra > altura:
+        y_cobra = 0
+    if y_cobra < 0:
+        y_cobra = altura
+    '''
+    
     if len(lista_cobra) > pontuacao + comprimento_inicial:
         del lista_cobra[0]
     aumenta_cobra(lista_cobra, cobra)
