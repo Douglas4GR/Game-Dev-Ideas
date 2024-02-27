e54481e841382cb6590182306817a867a0d30e34
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-02-27 00:47:52 -0300

    movimentação do sapo

--- pygame/primeiro_pj_sprites/sapo.py
+++ pygame/primeiro_pj_sprites/sapo.py
@@ -17,7 +17,7 @@ azul = (0, 0, 255)
 preto = (0, 0, 0)
 
 class Sapo(pygame.sprite.Sprite): #nome da classe tem que começar com letra maiúscula
-    def __init__(self):
+    def __init__(self): #método construtor
         pygame.sprite.Sprite.__init__(self)
         self.sprites = []
         self.sprites.append(pygame.image.load('pygame/primeiro_pj_sprites/sapos/attack_1.png'))
@@ -32,8 +32,22 @@ class Sapo(pygame.sprite.Sprite): #nome da classe tem que começar com letra mai
         self.sprites.append(pygame.image.load('pygame/primeiro_pj_sprites/sapos/attack_10.png'))
         self.atual = 0
         self.image = self.sprites[self.atual] #imagem inicial
+        self.image = pygame.transform.scale(self.image, (128*3, 64*3)) #tamanho da imagem
+        
         self.rect = self.image.get_rect() #pega o retângulo da imagem
         self.rect.topleft = 50, 50
+
+    def update(self): #método que atualiza a imagem
+        self.atual += 0.05
+        '''quanto maior o número acima, mais rápido a animação,
+        detalhe: esse número vai equivaler ao índice, logo se for menor que 1,
+        vai dar erro, pois não existe índice 0.5, por exemplo. '''
+        if self.atual >= len(self.sprites):
+            self.atual = 0
+        self.image = self.sprites[int(self.atual)]
+        ''' ^^^ atualiza a imagem ^^^
+        (para evitar o erro relatado anteriormente, é só converter o número para inteiro)'''
+        self.image = pygame.transform.scale(self.image, (128*3, 64*3)) #o tamanho da imagem deve ser mantido no loop
 
 #Objeto que irá armazenar todas as sprites do jogo (sapo, mosca, etc)
 todos_sprites = pygame.sprite.Group()
