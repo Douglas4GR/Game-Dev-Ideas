50bb391dd8284f12c634654a546f95cc98886669
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-03-03 18:57:31 -0300

    bonequinho roxo já tem imagem de fundo

--- pygame/jogo_do_bonequinho_roxo/bonequinho_roxo.py
+++ pygame/jogo_do_bonequinho_roxo/bonequinho_roxo.py
@@ -16,32 +16,34 @@ preto = (0, 0, 0)
 class Roxo(pygame.sprite.Sprite): #nome da classe tem que começar com letra maiúscula
     def __init__(self): #método construtor
         pygame.sprite.Sprite.__init__(self)
-        self.sprites = []
-        self.sprites.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_2.png'))
-        self.sprites.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_3.png'))
-        self.sprites.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_4.png'))
+        self.sprites_parado = []
+        self.sprites_parado.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_0.png'))
+        self.sprites_parado.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_1.png'))
+        self.sprites_andando = []
+        self.sprites_andando.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_1.png'))
+        self.sprites_andando.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_2.png'))
+        self.sprites_andando.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_3.png'))
+        self.sprites_andando.append(pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/roxo_andando_direita_4.png'))
         self.atual = 0
-        self.image = self.sprites[self.atual] #imagem inicial
+        self.image = self.sprites_parado[self.atual] #imagem inicial
         self.image = pygame.transform.scale(self.image, (64*3, 64*3)) #tamanho da imagem
         
         self.rect = self.image.get_rect() #pega o retângulo da imagem
         self.rect.topleft = 50, 50
 
-        self.animacao_ataque = False
-        self.barulho_ataque = pygame.mixer.Sound('pygame/primeiro_pj_sprites/sapos/barulho.wav')
+        self.barulho_andando = pygame.mixer.Sound('pygame/primeiro_pj_sprites/sapos/barulho.wav')
+        
 
-    def atacar(self):
-        self.animacao_ataque = True
-        self.barulho_ataque.play()
+    def andar_dir(self):
+        self.animacao_direita = True
+        self.barulho_andando.play()
 
     def update(self): #método que atualiza a imagem
-        if self.animacao_ataque == True:
-            self.atual += 1 #velocidade da animação (indice da lista de sprites)
-            if self.atual >= len(self.sprites):
-                self.atual = 0
-                self.animacao_ataque = False
-            self.image = self.sprites[int(self.atual)] #atualiza a imagem
-            self.image = pygame.transform.scale(self.image, (128*3, 64*3)) #o tamanho da imagem deve ser mantido no loop
+        self.atual += 0.05 #velocidade da animação (indice da lista de sprites)
+        if self.atual >= len(self.sprites_parado):
+            self.atual = 0
+        self.image = self.sprites_parado[int(self.atual)] #atualiza a imagem
+        self.image = pygame.transform.scale(self.image, (64*3, 64*3)) #o tamanho da imagem deve ser mantido no loop
 
 #Objeto que irá armazenar todas as sprites do jogo (roxo, mosca, etc)
 todos_sprites = pygame.sprite.Group()
@@ -49,7 +51,8 @@ todos_sprites = pygame.sprite.Group()
 #instanciando o roxo a partir da classe Roxo
 roxo = Roxo()
 todos_sprites.add(roxo)
-
+imagem_fundo = pygame.image.load('pygame/jogo_do_bonequinho_roxo/andando/campo.jpg').convert()
+imagem_fundo = pygame.transform.scale(imagem_fundo, (largura, altura))
 
 while True:
     relogio.tick(30)
@@ -61,8 +64,8 @@ while True:
         
         if event.type == KEYDOWN:
             if event.key == K_d:
-                roxo.atacar()
-
+                roxo.andar_dir()
+    tela.blit(imagem_fundo, (0, 0))
     todos_sprites.draw(tela)
     todos_sprites.update()
     pygame.display.flip() #o flip é melhor que o update
