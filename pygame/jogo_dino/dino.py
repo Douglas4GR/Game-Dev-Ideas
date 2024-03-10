c73a91bc17aafdfa2dbb589ba1f14d2db6b9cae9
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-03-10 20:17:13 -0300

    criando variáveis de controle do dino

--- pygame/jogo_dino/dino.py
+++ pygame/jogo_dino/dino.py
@@ -27,14 +27,18 @@ class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.imagens_dinossauro = []
+        self.indice_lista = 0
+        proporcoes_dino = 3
+        posic_inicial_X, posic_inicial_Y = 100, 200
+
         for i in range(3):
             frame_atual = sprite_sheet_1.subsurface(((i * 32),0),(32,32))
+            frame_atual = pygame.transform.scale(frame_atual, (32 * proporcoes_dino, 32 * proporcoes_dino))
             self.imagens_dinossauro.append(frame_atual)
         
-        self.indice_lista = 0
         self.image = self.imagens_dinossauro[self.indice_lista]
         self.rect = self.image.get_rect()
-        self.rect.center = (100,100)
+        self.rect.center = (posic_inicial_X,posic_inicial_Y)
 
     def update(self):
         if self.indice_lista > 2:
