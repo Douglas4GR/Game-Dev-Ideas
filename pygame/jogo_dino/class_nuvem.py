62f1969d0d0b5d1ce5d9bd69513f8a1076b012f2
Douglas Garcia Rangel <109640187+DouglasGR99@users.noreply.github.com>
2024-03-10 22:12:03 -0300

    agora a nuvemjá aparece no céu

--- pygame/jogo_dino/class_nuvem.py
+++ pygame/jogo_dino/class_nuvem.py
@@ -3,3 +3,11 @@ import pygame
 class Nuvem(pygame.sprite.Sprite):
     def __init__(self, sprite_sheet):
         pygame.sprite.Sprite.__init__(self)
+        proporcoes_nuvem = 3
+        posic_inicial_X, posic_inicial_Y = 50, 50
+        indice_X_no_sprite_sheet = 7 #a nuvem está na setima posicao da imagem
+        self.image = sprite_sheet.subsurface((32 * indice_X_no_sprite_sheet, 0), (32, 32))
+        self.image = pygame.transform.scale(self.image, (32 * proporcoes_nuvem, 32 * proporcoes_nuvem))
+        
+        self.rect = self.image.get_rect()
+        self.rect.center = (posic_inicial_X, posic_inicial_Y)
--- pygame/jogo_dino/main.py
+++ pygame/jogo_dino/main.py
@@ -5,28 +5,33 @@ import os
 from class_dino import Dino
 from class_nuvem import Nuvem
 
+
+#controlando os arquivos
 diretorio_main = os.path.dirname(__file__)
 diretorio_imagens = os.path.join(diretorio_main, 'imagens')
 diretorio_sons = os.path.join(diretorio_main, 'sons')
 
 # tela
-LARGURA = 640
-ALTURA = 480
+LARGURA, ALTURA = 640, 480
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 pygame.display.set_caption('Dino')
 todas_as_sprites = pygame.sprite.Group()
 relogio = pygame.time.Clock()
 
-# spritesheets
-sprite_sheet_1 = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpritesheet.png')).convert_alpha()
-
 # cores
 PRETO = (0, 0, 0)
 BRANCO = (255, 255, 255)
 
+# spritesheets
+sprite_sheet_1 = pygame.image.load(os.path.join(diretorio_imagens, 'dinoSpritesheet.png')).convert_alpha()
+
 # dino
 dinossauro = Dino(sprite_sheet_1)  # Cria uma instância da classe Dino
 todas_as_sprites.add(dinossauro)
+
+#nuvem
+nuvem = Nuvem(sprite_sheet_1)
+todas_as_sprites.add(nuvem)
 
 # loop principal
 while True:
